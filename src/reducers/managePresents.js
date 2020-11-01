ce40af6a1ce85823bf894327aa24365d915adc1b
amkera <60901491+amkera@users.noreply.github.com>
2020-11-01 17:59:09 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,21 @@
-export function managePresents(state, action){
+export function managePresents(state = {numberOfPresents: 0}, action){
+
+  switch(action.type) {
+    case 'INCREASE':
+      return Object.assign({}, state, {
+        numberOfPresents: state.numberOfPresents + 1
+      })
+      //Object.assign overwrites here since the key name is the same
+
+      default: return state;
+  }
+
 }
+
+
+//action = {type: "INCREASE"}
+
+//Object.assign(target, ...sources)
+// The Object.assign() copies all enumerable and own properties from
+// the source objects to the target object.
+// It returns the target object.
